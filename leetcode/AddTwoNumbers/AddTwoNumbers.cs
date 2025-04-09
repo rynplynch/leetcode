5417cbc1ea036beb6179673258ff96f5e4a1b9c1
Ryan Lynch <rynplynch@gmail.com>
2025-04-09 14:21:28 -0700

    Adding the first leetcode problem, with unfinished answer.

---
+++ leetcode/AddTwoNumbers/AddTwoNumbers.cs
@@ -0,0 +1,21 @@
+namespace Leetcode.AddTwoNumbers
+{
+    //Definition for singly-linked list.
+    public class ListNode {
+        public int val;
+        public ListNode? next;
+        public ListNode(int val=0, ListNode? next=null) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        public ListNode AddTwoNumbers(ListNode? l1, ListNode? l2)
+        {
+            ListNode answer = new();
+            return answer;
+        }
+    }
+}
